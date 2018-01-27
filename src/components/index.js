7695b255df62cbc2d70be64eb0cc45b5ab53f36c
Sosuke Suzuki <aosukeke@gmail.com>
2018-01-27 14:45:50 +0900

    add github link

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export default (state, actions) =>
   <div class={s.main}>
     <h1>hyperapp-boilerplate</h1>
     <p>hyperapp-boilerplate is a boilerplate for quickstarting a web application with Hyperapp, JSX, Stylus, Pug, Eslint.</p>
+    <p><a href="https://github.com/sosukesuzuki/hyperapp-boilerplate">github repository</a></p>
     <p>{state.count}</p>
     <button onclick={actions.plus}>+</button>
     <button onclick={actions.minus}>-</button>
