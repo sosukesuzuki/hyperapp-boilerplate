b2fbd992468779f17e8012a8eb6e81efe9482823
Sosuke Suzuki <aosukeke@gmail.com>
2018-01-27 14:35:40 +0900

    style fix

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ import s from './index.styl'
 
 export default (state, actions) =>
   <div class={s.main}>
+    <h1>hyperapp-boilerplate</h1>
+    <p>hyperapp-boilerplate is a boilerplate for quickstarting a web application with Hyperapp, JSX, Stylus, Pug, Eslint.</p>
     <p>{state.count}</p>
     <button onclick={actions.plus}>+</button>
     <button onclick={actions.minus}>-</button>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { app } from 'hyperapp'
 import actions from './actions'
 import state from './state'
 import view from './components'
+import 'normalize.css'
 
 export const main = app(
   state,
